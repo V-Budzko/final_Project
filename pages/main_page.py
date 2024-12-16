02aff7e1b3bbd36559428180d7ab4065b1f89e3c
V-Budzko <monodemonzzz@mail.ru>
2024-12-16 18:37:58 +0300

    improvment for test_guest_can_go_login_page()

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,6 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
-
+from .login_page import LoginPage
 from selenium.webdriver.common.by import By
 
 
@@ -11,6 +11,7 @@ class MainPage(BasePage):
 
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()   
+        #return LoginPage(self.browser, self.browser.current_url)  //Использован другой подход работы с методом should_be_login_page
           
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
